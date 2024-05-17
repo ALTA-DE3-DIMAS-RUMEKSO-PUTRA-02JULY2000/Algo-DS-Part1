95a4ec269ee265f24a3b57300cfb9677e19bbea7
masgal <dimasrumekso28@gmail.com>
2024-05-17 21:34:56 +0700

    solution5

--- problem5/main.py
+++ problem5/main.py
@@ -1,5 +1,11 @@
 def max_sequence(arr):
-    return 0
+    max_sum = max_current = arr[0]
+    
+    for num in arr[1:]:
+        max_current = max(num, max_current + num)
+        max_sum = max(max_sum, max_current)
+    
+    return max_sum
 
 if __name__ == "__main__":
     print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))  # 6
