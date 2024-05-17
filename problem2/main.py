b10667e5fbc424267050282561f4b84132af8fcd
masgal <dimasrumekso28@gmail.com>
2024-05-17 19:01:29 +0700

    problem2

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,20 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def primeX(x):
-    return 0
+    count = 0
+    num = 2
+    while True:
+        if is_prime(num):
+            count += 1
+            if count == x:
+                return num
+        num += 1
 
 if __name__ == "__main__":
     print(primeX(1))  # 2
